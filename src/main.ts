39e4a21c0ed742b14f95622500ca49e72e9359b0
nerdyEther <ether1926@gmail.com>
2024-11-24 03:24:35 +0530

    express import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import express from 'express';
-
+//import * as express from 'express';  //use this when working in development
 
 const expressApp = express();
 
